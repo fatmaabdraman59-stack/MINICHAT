24734e49a69c2888a5c20797bc0b408df2c220c2
Alice SEBEGO <alice.etvotrecom@gmail.com>
2021-06-19 23:33:28 +0200

    Graphic user's interface - WIP

--- index.php
+++ index.php
@@ -4,6 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS -->
+    <link rel="stylesheet" href="./dist/css/style.css">
+    <!-- script -->
+    <script defer src="./src/js/main.js"></script>
     <title>Mini-Chat</title>
 </head>
 <body>
@@ -27,7 +31,7 @@
         </form>
         <hr>
         <div id="feed">
-            <h2>Fil d'actu</h2>
+            <h2>Fil des messages</h2>
             <div id="feed-content">
             </div>
         </div>
