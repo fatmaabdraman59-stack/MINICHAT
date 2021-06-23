5946ddc1224153a01489ebe383025c0893b3328c
Alice SEBEGO <alice.etvotrecom@gmail.com>
2021-06-23 23:49:33 +0200

    Remote functions that are handle event's listening for user's input

--- src/js/main.js
+++ src/js/main.js
@@ -6,49 +6,7 @@ const $infoUser = document.querySelector("#infoUser");
 const $infoMsg = document.querySelector("#infoMsg");
 
 // Check input's pseudo
-$inputPseudo.addEventListener("input", () => {
-    
-    $infoUser.textContent = "";
-    
-    if($inputPseudo.value){
-        
-        if(util.regexPseudo.test($inputPseudo.value)){
-            if($infoUser.classList.contains("msg-error")){
-                $infoUser.classList.replace("msg-error", "msg-right")
-            } 
-            $infoUser.classList.add("msg-right");
-            $infoUser.textContent = `Votre pseudo est valide`;
-        } else {
-            if($infoUser.classList.contains("msg-right")){
-                $infoUser.classList.replace("msg-right", "msg-error")
-            } 
-            $infoUser.classList.add("msg-error");
-            $infoUser.textContent = `Votre pseudo n'est pas valide`;
-        }
-    }
-
-});
+util.validInputuser($inputPseudo, $infoUser);
 
 // Check input's message
-$inputMsg.addEventListener("input", () => {
-    
-    $infoMsg.textContent = "";
-    
-    if($inputMsg.value){
-        
-        if($inputMsg.value.length > 5 && $inputMsg.value.length < 255){
-            if($infoMsg.classList.contains("msg-error")){
-                $infoMsg.classList.replace("msg-error", "msg-right")
-            }
-            $infoMsg.classList.add("msg-right"); 
-            $infoMsg.textContent = `Votre message est d'une longueur valide`;
-        } else {
-            if($infoMsg.classList.contains("msg-right")){
-                $infoMsg.classList.replace("msg-right", "msg-error")
-            } 
-            $infoMsg.classList.add("msg-error");
-            $infoMsg.textContent = `Votre message est trop long ou trop court`;
-        }
-    }
-
-});
+util.validInputMessage($inputMsg, $infoMsg);
