f3f188a149800b072cc5134ed12db5ee752983cf
Alice SEBEGO <alice.etvotrecom@gmail.com>
2021-06-29 16:34:25 +0200

    Add a favicon

--- view/view-head.php
+++ view/view-head.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="<?= $infoPages['chat']['metadesc'] ?>">
+    <link rel="icon" type="image/png" href="./assets/chat.png" />
     <!-- CSS -->
     <link rel="stylesheet" href="./dist/css/style.css">
     <!-- script -->
